c3cab14312a939f0012c51e139906d1ec8013fe4
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-08-07 23:54:10 -0300

    feat: add success page

--- src/components/Finish/index.js
+++ src/components/Finish/index.js
@@ -1,15 +1,36 @@
 import "./styles.css";
 export default function Finish({ isWrong, setActualPage }) {
   console.log("wrong:", isWrong);
-
+  let answer = null;
+  if (isWrong) {
+    answer = {
+      title: "Putz..",
+      subtitle:
+        "Você esqueceu alguns flashcards... Não desanime! Na próxima você consegue!",
+      img: "https://i.imgur.com/8lIHmXk.png",
+    };
+  }
+  if (!isWrong) {
+    answer = {
+      title: "PARABÉNS!",
+      subtitle: "Você não esqueceu de nenhum flashcard!",
+      img: "https://i.imgur.com/jTtnfll.png",
+    };
+  }
   return (
     <div className="inicio">
       <div className="navBar">
-        <img class="logo-image" src="https://i.imgur.com/3ooqvtH.png"></img>
+        <img src="https://i.imgur.com/3ooqvtH.png"></img>
       </div>
-      <section>
-        <div className="logo-card">Fimmmmmmmmmmmm</div>
-      </section>
+      <div className="container">
+        <div className="result">
+          <h1>
+            {answer.title}
+            <img className="logo-result" src={answer.img}></img>
+          </h1>
+          <h2>{answer.subtitle}</h2>
+        </div>
+      </div>
     </div>
   );
 }
